a99d0a16db8b4c8e6551dcad7f9f35fb550291b3
maria-moroz <mma.moroz.mariia@gmail.com>
2022-06-14 00:58:04 +0300

    update

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import s from './Button.module.css';
+
+export default function Button({ text, onClick }) {
+  return (
+    <button type="button" className={s.button} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
