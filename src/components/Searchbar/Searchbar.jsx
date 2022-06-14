ce3ab402c9bbb9cf2ea211a710ddb70355ed04e7
maria-moroz <mma.moroz.mariia@gmail.com>
2022-06-14 19:43:36 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -23,12 +23,6 @@ class Searchbar extends Component {
 
     e.preventDefault();
     onSubmit(filter);
-
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ filter: '' });
   };
 
   render() {
